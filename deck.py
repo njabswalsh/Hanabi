d90360280cadd432b9cfc5235945521c323f72e2
cjabswalsh <cjwalsh314@gmail.com>
2018-01-03 23:31:07 -0800

    made deck a Hanabi deck

--- deck.py
+++ deck.py
@@ -3,8 +3,8 @@ from card import Card
 class Deck:
 	def __init__(self):
 		self.cards = []
-		self.colors = ["red", "blue"]
-		self.numbers = [1, 2, 3, 4, 5]
+		self.colors = ["red", "blue", "green", "purple", "brown"]
+		self.numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
 		self.fill_deck()
 
 	def fill_deck(self):
