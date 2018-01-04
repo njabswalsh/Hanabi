07c4b7f6d96ea3268af926268bf28fa7ad56ce8f
njabswalsh <njwalsh@stanford.edu>
2018-01-03 22:59:34 -0800

    adding card and main

---
+++ card.py
@@ -0,0 +1,4 @@
+class Card:
+  def __init__(self, color, number):
+    self.color = color
+    self. number = number
---
+++ main.py
@@ -0,0 +1,4 @@
+from card import Card
+
+card = Card("red", 3)
+print(card.color)
