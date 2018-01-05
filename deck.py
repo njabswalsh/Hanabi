2b9bc42d216b50ba895abe114d50b1a04af91ebd
njabswalsh <njwalsh@stanford.edu>
2018-01-04 18:31:24 -0800

    changing class variables

--- deck.py
+++ deck.py
@@ -1,13 +1,14 @@
 from card import Card
 
 class Deck:
-	def __init__(self):
-		self.cards = []
-		self.colors = ["red", "blue", "green", "purple", "brown"]
-		self.numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
-		self.fill_deck()
+  colors = ["red", "blue", "green", "purple", "brown"]
+  numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
 
-	def fill_deck(self):
-		for color in self.colors:
-			for number in self.numbers:
-				self.cards.append(Card(color, number))
+  def __init__(self):
+    self.cards = []
+    self.fill_deck()
+
+  def fill_deck(self):
+    for color in self.colors:
+      for number in self.numbers:
+        self.cards.append(Card(color, number))
