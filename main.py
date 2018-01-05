8abf61ec21318745092a30d8025ef2e10bbd42ae
cjabswalsh <cjwalsh314@gmail.com>
2018-01-04 19:03:28 -0800

    testing draw, shuffle and print

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from card import Card
 from deck import Deck
 
 deck = Deck()
-for card in deck.cards: 
-	print(str(card.number) + " of " + card.color)
+# for card in deck.cards: 
+# 	print(str(card.number) + " of " + card.color)
+deck.shuffle()
+
+deck.print_deck()
+
+print("\n")
+
+hand = deck.draw(5)
+for i in hand:
+  print(str(i.number) + " of " + i.color)
+
+print
+
+deck.print_deck()
 
 #card = Card("red", 3)
 #print(card.color)
