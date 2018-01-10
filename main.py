29024fe816653aa737f900c10d33c5acfc40c5f4
BRYCE WALSH <bryce.j.walsh@gmail.com>
2018-01-10 13:24:59 -0800

    Implemented playing/played part of table class and edited main to test it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from card import Card
 from deck import Deck
+from table import Table
 
 deck = Deck()
 
@@ -17,5 +18,12 @@ print
 
 deck.print_deck()
 
+print("TABLE STUFF")
+table = Table()
+table.print_state()
+print
+one_of_brown = Card("brown",1)
+table.play_card(one_of_brown)
+table.print_state()
 #card = Card("red", 3)
 #print(card.color)
--- table.py
+++ table.py
@@ -1,3 +1,29 @@
 class Table:
   def __init__(self, played=None, discarded=None):
+    # if(played)
+    #   self.played = played
+    # else
+    self.played = {"red":0,"green":0,"blue":0,"brown":0,"purple":0}
+    # if(discarded)
+    #   self.discarded = discarded
+    # else
+    self.discarded = {}
+
+  # Returns 1 if card is playable, 0 otherwise
+  def play_card(self, card):
+    if (self.is_playable(card)):
+      self.played[card.color] += 1 
+      return 1
+    else:
+      return 0 
     
+  def print_state(self):
+    print("Played:")
+    for key in self.played:
+      print(str(self.played[key]) + " of " + key)
+
+  def is_playable(self, card):
+    playable = False
+    if (self.played[card.color] == card.number - 1):
+      playable = True  
+    return playable
